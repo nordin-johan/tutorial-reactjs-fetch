14359c52eff6014fd3f9454c706eb0f67b00205a
nordin <nordinjohan89@gmail.com>
2019-12-08 00:06:48 +0100

    init commit!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/**
+ * App
+ *
+ * Simple react js fetch example
+ */
+class App extends React.Component {
+
+    /**
+     * constructor
+     *
+     * @object  @props  parent props
+     * @object  @state  component state
+     */
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            items: [],
+            isLoaded: false
+        }
+
+    }
+
+    /**
+     * componentDidMount
+     *
+     * Fetch json array of objects from given url and update state.
+     */
+    componentDidMount() {
+
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    items: json,
+                    isLoaded: true, 
+                })
+            }).catch((err) => {
+                console.log(err);
+            });
+
+    }
+
+    /**
+     * render
+     *
+     * Render UI
+     */
+    render() {
+
+        const { isLoaded, items } = this.state;
+
+        if (!isLoaded)
+            return <div>Loading...</div>;
+
+        return (
+            <div className="App">
+                <ul>
+                    {items.map(item => (
+                        <li key={item.id}>
+                            Name: {item.name} | Email: {item.email}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+
+    }
+
 }
 
 export default App;
